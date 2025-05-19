c7ffcc2faa4445d9c8a0985cab8b431f6bad670e
omer-siraj <omerdinsiraj@gmail.com>
2025-05-18 19:08:18 -0700

    axios and requests and env

---
+++ addis-netflix-clone/src/utils/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+// Create a configured axios instance for TMDB API
+const instance = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+
+});
+
+export default instance;
