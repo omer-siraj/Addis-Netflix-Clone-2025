923870febc23d9049b522b48f6dde1c727ce5a02
omer-siraj <omerdinsiraj@gmail.com>
2025-05-21 20:12:28 -0700

    updated

--- addis-netflix-clone/src/components/Header/Header.js
+++ addis-netflix-clone/src/components/Header/Header.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import './header.css'
-import NetflixLogo from '../../assets/images/netflix_PNG6.png'
+import React from 'react';
+import './header.css';
+import NetflixLogo from '../../assets/images/netflix_PNG6.png';
+
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
@@ -9,29 +10,31 @@ import AccountBoxIcon from '@mui/icons-material/AccountBox';
 const Header = () => {
   return (
     <div className='header_outer_container'>
-        <div className='header_container'>
-            <div className='header_left'>
-                <ul>
-                    <li><img src={NetflixLogo} alt='Netflix Logo' width='100' /> </li>
-                    <li>Netflix</li>
-                    <li>Home</li>
-                    <li>TVShows</li>
-                    <li>Movies</li>
-                    <li>Latest</li>
-                    <li>MyList</li>
-                    <li>Browse by Languages</li>
-                </ul>
-             </div>
-            <div className='header_right'>
-                <ul>
-                    <li><SearchIcon /></li>
-                    <li><NotificationsNoneIcon /></li>
-                    <li><AccountBoxIcon /></li>
-                    <li><ArrowDropDownIcon /></li>
-                </ul>
-            </div>
+      <div className='header_container'>
+        <div className='header_left'>
+          <img src={NetflixLogo} alt='Netflix Logo' className='netflix_logo' />
+          <nav className='nav_links'>
+            <ul>
+              <li>Home</li>
+              <li>TV Shows</li>
+              <li>Movies</li>
+              <li>Latest</li>
+              <li>My List</li>
+              <li>Browse by Languages</li>
+            </ul>
+          </nav>
         </div>
+        <div className='header_right'>
+          <ul>
+            <li><SearchIcon /></li>
+            <li><NotificationsNoneIcon /></li>
+            <li><AccountBoxIcon /></li>
+            <li><ArrowDropDownIcon /></li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
-export default Header
+  );
+};
+
+export default Header;
